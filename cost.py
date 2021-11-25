405d6adfe1354a87b27e0b6d8091bab55413663c
EC2 Default User <ec2-user@ip-172-31-17-63.ec2.internal>
2021-11-25 02:03:39 +0000

    this is the first commit

---
+++ cost.py
@@ -0,0 +1,15 @@
+"""
+Exercise 2
+
+An avocado spread costs $4, and a cup of coffee costs $2.
+
+Instructions:
+1. create the variables, avocado, coffee, and store the prices in each variable, respectively. 
+2. create the variable, breakfast, and calculate the total cost of an avocado spread and a cup of coffee
+3. display the total in the output window
+"""
+avocado = 4
+coffee = 2
+
+breakfast = avocado + coffee
+print(breakfast)
