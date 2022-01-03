93c2c512808f0f9eb4d86c30849b49a88531b0cb
Alemmaya <alemmayamulugeta@outlook.com>
2022-01-02 19:42:43 -0500

    today

--- concat/exercise1.py
+++ concat/exercise1.py
@@ -2,3 +2,5 @@
 name=  input("your name:   ")
 email= input("your email:  ")
 phone= input("your phone:   ")
+
+print(f"you are the best {name}")
---
+++ loop/exercise2.py
@@ -0,0 +1,10 @@
+clist = ['Canada','USA','Mexico','Australia']
+"""
+Create a loop that counts from 0 to 100
+Make a multiplication table using a loop
+Output the numbers 1 to 10 backwards using a loop
+Create a loop that counts all even numbers to 10
+Create a loop that sums the numbers from 100 to 200
+"""
+for country in clist:
+    print(country)
